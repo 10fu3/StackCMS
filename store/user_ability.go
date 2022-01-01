4595d5f9efc136adf94906997cda4982af14fcab
10fu3 <msf.1192@gmail.com>
2022-01-02 02:20:00 +0900

    fixed sql params

--- store/user_ability.go
+++ store/user_ability.go
@@ -1,18 +1,38 @@
 package store
 
-import "StackCMS/model"
+import (
+	"StackCMS/model"
+	"github.com/jmoiron/sqlx"
+)
 
-func (d *Db) IsUserAuthorization(userId string, ability model.Ability) bool {
-	r, e := d.Db.Query("select * from "+
-		"role_ability "+
-		"inner join "+
-		"user_role "+
-		"on "+
-		"role_ability.role_id = role_ability.role_id "+
-		"where "+
-		"user_id = ? "+
-		"and "+
-		"ability_id = ?", userId, ability.String())
+func (d *Db) IsUserAuthorization(userId string, abilities []model.Ability) bool {
+
+	args := func() []string {
+		a := []string{}
+		for _, ability := range abilities {
+			a = append(a, ability.String())
+		}
+		return a
+	}()
+
+	sql := "select * from " +
+		"role_ability " +
+		"inner join " +
+		"user_role " +
+		"on " +
+		"role_ability.role_id = role_ability.role_id " +
+		"where " +
+		"user_id = ? " +
+		"and " +
+		"ability_id IN (?)"
+
+	q, a, e := sqlx.In(sql, userId, args)
+
+	if e != nil {
+		return false
+	}
+
+	r, e := d.Db.Query(q, a...)
 	if e != nil {
 		return false
 	} else {
