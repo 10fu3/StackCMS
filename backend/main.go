16fd1d179553ab4872d84eaf80a16ea0454d4b5a
10fu3 <msf.1192@gmail.com>
2022-02-07 16:28:46 +0900

    fix any

--- backend/main.go
+++ backend/main.go
@@ -7,6 +7,7 @@ import (
 	"fmt"
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
+	"net/http"
 )
 
 func main() {
@@ -19,20 +20,23 @@ func main() {
 
 	err := Setup.Db()
 
-	if err != nil {
-		fmt.Println(err.Error())
-		return
-	}
-
 	g := gin.Default()
 
-	// CORS 対応
-	corsConfig := cors.DefaultConfig()
-	corsConfig.AllowOrigins = []string{"*"}
-	corsConfig.AllowHeaders = []string{"authorization"}
-	g.Use(cors.New(corsConfig))
+	if err == nil {
+		// CORS 対応
+		corsConfig := cors.DefaultConfig()
+		corsConfig.AllowOrigins = []string{"*"}
+		corsConfig.AllowHeaders = []string{"authorization"}
+		g.Use(cors.New(corsConfig))
 
-	router.RegisterRoute(g)
+		router.RegisterRoute(g)
+	} else {
+		g.GET("/", func(ctx *gin.Context) {
+			ctx.JSON(http.StatusOK, gin.H{
+				"message": "config_error",
+			})
+		})
+	}
 
 	g.Run(fmt.Sprintf(":%s", config.Values.AppPort))
 }
